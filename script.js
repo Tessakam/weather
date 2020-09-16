6e22e82a89a73c0d6919b9c88040836596671dcb
Tessakam <tessa.kam@hotmail.com>
2020-09-16 12:46:55 +0200

    update - lunch // fetch not working yet

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 (() => {
 
-    document.getElementById("search").addEventListener("click", (fetchWeather) => {
-            console.log("test")
+    let url = "http://api.openweathermap.org/data/2.5/forecast?q="
+    const myKey = "4b8d4fe8a28fa00a04cf4d9ee056c183";
+    let input = document.getElementById("input").innerHTML;
 
-            function fetchWeather() {
-                let input = document.getElementById("input").value;
-                let url = "http://api.openweathermap.org/data/2.5/forecast?q=" + input + "&APPID="
-                const myKey = "4b8d4fe8a28fa00a04cf4d9ee056c183";
-            //example http://api.openweathermap.org/data/2.5/forecast?q=Mechelen&APPID=4b8d4fe8a28fa00a04cf4d9ee056c183
+    function search() {
+        document.getElementById("search").addEventListener("click", function () {
+            return input
+        })
+        console.log(input)
 
-                fetch(url + myKey)
-                    .then(response => response.json())
-                    .then(data => console.log(data.list))
-
-                    .catch(function (error) {
-                        alert("ERROR!\nPlease check your input!\nMake sure this is a valid city")
-                        console.log("error, not found")
-                    })
-            }
+        //example http://api.openweathermap.org/data/2.5/forecast?q=Mechelen&APPID=4b8d4fe8a28fa00a04cf4d9ee056c183
+        function fetchWeather(search) {
+            fetch(url + input + "&APPID=" + myKey) //parameters
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    document.getElementById("nameLocation").innerHTML = "Location: " + data.city.name
+                })
+            console.log(data)
         }
-    )
+    }
 })
 ();
 
 /*
-document.getElementById("input").innerHTML = "City: " + data.city.name;
+.catch(function (error) {
+alert("ERROR!\nPlease check your input!\nMake sure this is a valid city")
+console.log("error, not found")
+})
  */
