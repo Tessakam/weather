262b6c7e45a015c4be1e29e95aa84d423a3ecef3
Tessakam <tessa.kam@hotmail.com>
2020-09-17 15:21:32 +0200

    converte dt into readable time with timeConverter

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
     let secretkey = config.MY_KEY;
     let url = "http://api.openweathermap.org/data/2.5/forecast?q=";
-    let today = new Date()
 
     document.getElementById("search").addEventListener("click", search);
 
@@ -18,11 +17,10 @@
                 })
                 .then(data => {
                     getName(data);
-                    getToday(data);
-                    /*getTemperatureDay2(data);
-                    getTemperatureDay3(data);
-                    getTemperatureDay4(data);
-                    getTemperatureDay5(data);*/
+                    getDays(data);
+                    timeConverter(UNIX_timestamp)
+
+
                 })
 
             /*.catch(error => {
@@ -36,15 +34,42 @@
             document.getElementById("nameLocation").innerHTML = "The weather in " + name
         }
 
-        //add loop because data is in array
-        function getToday(data) {
-            for (let i = 0; i < data.length; i++) {
-                let today = [];
-                today.push(data["list"][i]["dt_text"])
-                console.log(today)
-            }
+        //translate dt-text into today, day 1, ...
+        //Example: https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript/847196#847196
+        function timeConverter(UNIX_timestamp){
+            let a = new Date(UNIX_timestamp * 1000);
+            let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+            let year = a.getFullYear();
+            let month = months[a.getMonth()];
+            let date = a.getDate();
+            let hour = a.getHours();
+            let min = a.getMinutes();
+            let sec = a.getSeconds();
+            let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+            return time;
+        }
+        console.log(timeConverter(0));
+
+
+            /*//add loop because data is in array
+            function getDays(data) {
+                let today = new Date()
+                let day1 = [];
+                let day2 = [];
+                let day3 = [];
+                let day4 = [];
+                let day5 = [];
+
+                for (let i = 0; i < data.length; i++) {
+                    let today = [];
+                    today.push(data["list"][i]["dt_text"])
+                    console.log(today)
+                }
+            }*/
+
         }
     }
-})
+
+)
 ();
 //Example http://api.openweathermap.org/data/2.5/forecast?q=antwerp&units=metric&APPID=key
